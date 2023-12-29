76d445aaf748e3283612bf4256727dd7b34e0d00
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2023-12-29 18:19:51 +0900

    fix

--- src/pages/api/db/circle.ts
+++ src/pages/api/db/circle.ts
@@ -2,14 +2,18 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getAllCircles } from "@/lib/db";
 import { CircleWithID } from '@/lib/types';
 
-type ResponseData = CircleWithID[];
+type ResponseData = {
+  [key: string]: CircleWithID
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   const circles = await getAllCircles();
+
+  const circleRes = Object.fromEntries(circles.map((circle) => [circle.id, circle]));
   res
     .status(200)
-    .json(circles)
+    .json(circleRes)
 }
